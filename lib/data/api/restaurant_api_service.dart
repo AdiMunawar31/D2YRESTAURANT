6fd2112607f742d3b599911e0010f818c6e70a78
Adi Munawar <munawar.adi31@gmail.com>
2022-03-30 00:20:49 +0700

    implement try and catch

--- lib/data/api/restaurant_api_service.dart
+++ lib/data/api/restaurant_api_service.dart
@@ -6,11 +6,15 @@ class RestaurantApiService {
   static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   Future<RestaurantResult> getRestaurantList() async {
-    final response = await http.get(Uri.parse(_baseUrl + 'list'));
-    if (response.statusCode == 200) {
-      return RestaurantResult.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load restaurant list');
+    try {
+      final response = await http.get(Uri.parse(_baseUrl + 'list'));
+      if (response.statusCode == 200) {
+        return RestaurantResult.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to load restaurant list');
+      }
+    } catch (err) {
+      throw Exception(err);
     }
   }
 }
--- lib/data/api/search_restaurant_api_service.dart
+++ lib/data/api/search_restaurant_api_service.dart
@@ -6,11 +6,15 @@ class SearchRestaurantApiService {
   static const String _baseUrl = 'https://restaurant-api.dicoding.dev/';
 
   Future<SearchRestaurantResult> getRestaurantSearch(String query) async {
-    final response = await http.get(Uri.parse(_baseUrl + 'search?q=$query'));
-    if (response.statusCode == 200) {
-      return SearchRestaurantResult.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception('Failed to load restaurant search');
+    try {
+      final response = await http.get(Uri.parse(_baseUrl + 'search?q=$query'));
+      if (response.statusCode == 200) {
+        return SearchRestaurantResult.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to load restaurant search');
+      }
+    } catch (err) {
+      throw Exception(err);
     }
   }
 }
